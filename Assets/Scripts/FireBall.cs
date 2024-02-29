d2308c354768542b674ac870b751a4afa77bf92a
Charizma <musin8922404@gmail.com>
2024-02-29 18:59:33 +0500

    q

---
+++ Assets/Scripts/FireBall.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBall : MonoBehaviour
+{
+
+    public float speed;
+    public float lifetime;
+
+    private void FixedUpdate()
+    {
+       MoveFixedUpdate();
+    }
+
+    private void MoveFixedUpdate()
+    {
+        transform.position += transform.forward * speed * Time.fixedDeltaTime;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        DestroyFireBall();
+    }
+
+    private void DestroyFireBall()
+    {
+        Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        Invoke ("DestroyFireBall", lifetime);
+    }
+
+}
---
+++ Assets/Scripts/FireBallCaster.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBallCaster : MonoBehaviour
+{
+
+    public FireBall fireballPrefab;
+    public Transform fireballSourceTransform;
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Instantiate(fireballPrefab, fireballSourceTransform.position, fireballSourceTransform.rotation);
+        }
+    }
+}
